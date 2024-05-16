0be3c0114cb312a9dfdf75d9495da64ba47dada6
Waqar-Rajpoot <rajpoottony61@gmail.com>
2024-05-15 23:40:37 -0700

    new project

--- Contact Manager/src/App.jsx
+++ Contact Manager/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Header from "./Components/Header"
 import ContactList from "./Components/ContactList"
 import AddContact from "./Components/AddContact"
@@ -6,24 +6,32 @@ import uuid4 from 'uuid4';
 
 function App() {
 
-  const [contact, setContact] = useState([]);
+  const localStorageKey = "contact";
+  const [contact, setContact] = useState(() => {
+    return JSON.parse(localStorage.getItem(localStorageKey))
+      || []
+  });
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(contact))
+  }, [contact])
+
 
   const addContact = (data) => {
-    setContact([...contact, {id: uuid4(), data}])
+    setContact([...contact, { id: uuid4(), data }])
   }
 
   const removeContact = (id) => {
     const updatedList = contact.filter((val) => {
-      return val.id!== id;
+      return val.id !== id;
     })
     setContact(updatedList)
   }
   return (
     <>
-    <Header />
-    <AddContact addContact = {addContact}/>
-    <ContactList contact={contact} removeContact={removeContact}/>
-      
+      <Header />
+      <AddContact addContact={addContact} />
+      <ContactList contact={contact} removeContact={removeContact} />
+
     </>
   )
 }
